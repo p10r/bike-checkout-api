aca761b3762b5406395df8db9d28008141ea6c58
Philipp Mayer <phlppmayer@gmail.com>
2023-05-23 09:16:25 +0200

    use event decorators

--- diagrams/http4k-diagrams/src/main/kotlin/de/p10r/App.kt
+++ diagrams/http4k-diagrams/src/main/kotlin/de/p10r/App.kt
@@ -1,7 +1,13 @@
 package de.p10r
 
-import de.p10r.GetFinancingResult.*
-import de.p10r.OrderResult.*
+import de.p10r.GetFinancingResult.BikeNotFound
+import de.p10r.GetFinancingResult.Rates
+import de.p10r.GetFinancingResult.RatesNotAvailable
+import de.p10r.OrderResult.AlreadyOrdered
+import de.p10r.OrderResult.BikeNotInStock
+import de.p10r.OrderResult.CannotBeFinanced
+import de.p10r.OrderResult.ServerError
+import de.p10r.OrderResult.Success
 import de.p10r.outgoing.HttpBankClient
 import de.p10r.outgoing.HttpWarehouse
 import de.p10r.outgoing.OrderRepository
@@ -19,6 +25,7 @@ import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Uri
 import org.http4k.core.then
 import org.http4k.core.with
+import org.http4k.events.Events
 import org.http4k.filter.DebuggingFilters.PrintRequest
 import org.http4k.filter.ServerFilters
 import org.http4k.format.Jackson.auto
@@ -31,30 +38,33 @@ val orderBikeRequest = Body.auto<OrderBikeRequest>().toLens()
 val id = Path.map(::BikeId).of("id")
 
 fun App(
+    events: Events,
     bankUri: Uri,
     bankHttp: HttpHandler,
     warehouseUri: Uri,
     warehouseHttp: HttpHandler,
     db: OrderRepository,
 ): HttpHandler {
+    val appEvents = AppEvents("checkout-api", events)
+
     val checkout = CheckoutApi(
-        HttpBankClient(bankUri, bankHttp),
-        HttpWarehouse(warehouseUri, warehouseHttp),
+        HttpBankClient(bankUri, OutgoingHttp(appEvents, bankHttp)),
+        HttpWarehouse(warehouseUri, OutgoingHttp(appEvents, warehouseHttp)),
         db
     )
 
     return PrintRequest()
         .then(ServerFilters.CatchAll())
         .then(ServerFilters.CatchLensFailure())
-        .then(ApiRoutes(checkout))
+        .then(IncomingHttp(events, ApiRoutes(checkout)))
 }
 
 private fun ApiRoutes(checkout: CheckoutApi) = routes(
     "/bikes/{id}/rates" bind GET to { req ->
         when (val result = checkout.listFinancingRates(id(req))) {
-            is BikeNotFound      -> Response(NOT_FOUND)
+            is BikeNotFound -> Response(NOT_FOUND)
             is RatesNotAvailable -> Response(NOT_FOUND)
-            is Rates             -> Response(OK).with(ratesLens of result.rates)
+            is Rates -> Response(OK).with(ratesLens of result.rates)
         }
     },
     "/bikes/{id}/order" bind POST to { req: Request ->
@@ -67,11 +77,11 @@ private fun ApiRoutes(checkout: CheckoutApi) = routes(
                 chosenFinancingRate = order.financingRate
             )
         ) {
-            is BikeNotInStock   -> Response(NOT_FOUND)
+            is BikeNotInStock -> Response(NOT_FOUND)
             is CannotBeFinanced -> Response(CONFLICT)
-            is Success          -> Response(CREATED)
-            is AlreadyOrdered   -> Response(OK)
-            is ServerError      -> Response(INTERNAL_SERVER_ERROR)
+            is Success -> Response(CREATED)
+            is AlreadyOrdered -> Response(OK)
+            is ServerError -> Response(INTERNAL_SERVER_ERROR)
         }
     }
 )
--- diagrams/http4k-diagrams/src/test/kotlin/de/p10r/Fixture.kt
+++ diagrams/http4k-diagrams/src/test/kotlin/de/p10r/Fixture.kt
@@ -5,8 +5,9 @@ import de.p10r.fakes.FakeOrderRepository
 import de.p10r.fakes.FakeWarehouse
 import de.p10r.outgoing.WarehouseResponse
 import org.http4k.core.Uri
+import org.http4k.events.Events
 
-fun CheckoutApi(
+fun TestApp(
     stock: List<WarehouseResponse> = listOf(
         WarehouseResponse(
             id = "111",
@@ -14,7 +15,9 @@ fun CheckoutApi(
             price = 550.0,
         )
     ),
+    events: Events = {}
 ) = App(
+    events,
     bankUri = Uri.of("http://bank"), bankHttp = FakeBank(),
     warehouseUri = Uri.of("http://warehouse"), warehouseHttp = FakeWarehouse(stock),
     db = FakeOrderRepository()
